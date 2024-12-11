f58c80795168bed8fd208a57255d76bb89acc015
Nadia <oulhine04@gmail.com>
2024-12-11 12:45:34 +0100

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+const sidebar = document.getElementById("side-bar");
+const content = document.querySelector(".content");
+
+btnt.addEventListener("click", ()=>{
+    sidebar.classList.toggle("active");
+})
